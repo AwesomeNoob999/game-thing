2e5092ce0e1f09ae1514315343a843b2900e81e7
AwesomeNoob999 <51246943+AwesomeNoob999@users.noreply.github.com>
2021-04-23 14:13:39 -0500

    Create main_game_thing.rb

---
+++ main_game_thing.rb
@@ -0,0 +1,22 @@
+require "./map.rb"
+
+puts "what your name"
+print "> "
+
+ee = $stdin.gets.chomp
+
+puts "hello #{ee}, lets begin your journey into the dungeons."
+
+puts "there is a door in front of you. Will you go in?"
+
+print "> "
+choice = $stdin.gets.chomp
+
+
+
+if choice == "e"
+Rooms.start_room
+elsif choice == "no"
+puts "you walk away and forget all about it"
+exit(0)
+end
